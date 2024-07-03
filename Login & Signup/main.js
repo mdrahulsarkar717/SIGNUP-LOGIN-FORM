e1343b9ffcb867a8add2c93ff05a87f12c7d5177
rahul787878 <mdrahulsarkar717@gmail.com>
2024-07-03 09:20:24 +0600

    update

---
+++ Login & Signup/main.js
@@ -0,0 +1,3 @@
+let signup = document.querySelector('#signup-link');
+let login = document.querySelector('#login-link');
+
---
+++ MAIN/index.js
@@ -0,0 +1,7 @@
+let heros= ["Tony Stark","Thor","Superman","Mr. America","Thanos","Spiderman"," Hulk"];
+
+while (let  idx = 0;  idx < heros.lentgh; i++){
+    console.log(heros[idx]);
+}/* else {
+   console.log("Error");
+};*/
